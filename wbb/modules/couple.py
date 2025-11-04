300d437be2bb8e8af6c7b8613c22fdd43ab91285
Prakhar Shukla <SmokenX1@gmail.com>
2025-11-04 10:39:54 +0530

    Update couple.py

--- wbb/modules/couple.py
+++ wbb/modules/couple.py
@@ -32,7 +32,7 @@ from wbb.core.decorators.errors import capture_err
 from wbb.utils.dbfunctions import get_couple, save_couple
 
 __MODULE__ = "Shippering"
-__HELP__ = "/detect_gay - To Choose Couple Of The Day"
+__HELP__ = "/couple - To Choose Couple Of The Day"
 
 
 # Date and time
@@ -67,13 +67,13 @@ def tomorrow():
     return str(dt_tom())
 
 
-@app.on_message(filters.command("detect_gay"))
+@app.on_message(filters.command("couple"))
 @capture_err
 async def couple(_, message):
     if message.chat.type == enums.ChatType.PRIVATE:
         return await message.reply_text("This command only works in groups.")
 
-    m = await message.reply("Detecting gay among us...")
+    m = await message.reply("Detecting gays among us...")
 
     try:
         chat_id = message.chat.id
