159581e4ee45a3ba7bcab7bdbea4b7e1809e3018
Prakhar Shukla <SmokenX1@gmail.com>
2025-11-05 11:26:39 +0530

    Update autocorrect.py

--- wbb/modules/autocorrect.py
+++ wbb/modules/autocorrect.py
@@ -1,11 +1,11 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from wbb import SUDOERS, USERBOT_ID, USERBOT_PREFIX, app, app2, arq
+from wbb import SUDOERS, USERBOT_ID, USERBOT_PREFIX, app, app2, arq, HAS_USERBOT
 from wbb.modules.userbot import eor
 from wbb.utils.filter_groups import autocorrect_group
 
-
+# ---- bot-side handler (unchanged) ----
 @app.on_message(filters.command("autocorrect"))
 async def autocorrect_bot(_, message: Message):
     if not message.reply_to_message:
@@ -23,14 +23,25 @@ async def autocorrect_bot(_, message: Message):
 
 IS_ENABLED = False
 
+# ---- userbot decorator shim (no-op if userbot is disabled) ----
+if app2 is not None and HAS_USERBOT:
+    ubot_on_message = app2.on_message
+else:
+    def ubot_on_message(*args, **kwargs):
+        def _wrap(func):
+            return func
+        return _wrap
 
-@app2.on_message(
+
+@ubot_on_message(
     filters.command("autocorrect", prefixes=USERBOT_PREFIX)
     & ~filters.forwarded
     & ~filters.via_bot
     & SUDOERS
 )
 async def autocorrect_ubot_toggle(_, message: Message):
+    if app2 is None:
+        return
     global IS_ENABLED
     if len(message.command) != 2:
         return await eor(message, text="Not enough arguments.")
@@ -47,7 +58,7 @@ async def autocorrect_ubot_toggle(_, message: Message):
         )
 
 
-@app2.on_message(
+@ubot_on_message(
     filters.text
     & filters.user(USERBOT_ID)
     & ~filters.forwarded
@@ -55,6 +66,8 @@ async def autocorrect_ubot_toggle(_, message: Message):
     group=autocorrect_group,
 )
 async def autocorrect_ubot(_, message: Message):
+    if app2 is None:
+        return
     if not IS_ENABLED:
         return
     text = message.text
